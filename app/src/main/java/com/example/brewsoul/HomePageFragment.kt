3c01646c30f73855087f026a35e1c95a6888c4e2
Andres Austin <andresaustin20@gmail.com>
2024-12-07 12:01:49 +0530

    viewmodel integration
    - seperated ui data and logic from UI to Viewmodel

--- app/src/main/java/com/example/brewsoul/HomePageFragment.kt
+++ app/src/main/java/com/example/brewsoul/HomePageFragment.kt
@@ -1,10 +1,12 @@
 package com.example.brewsoul
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.brewsoul.databinding.FragmentHomePageBinding
 
 
@@ -13,22 +15,8 @@ class HomePageFragment : Fragment() {
     private lateinit var _binding: FragmentHomePageBinding
     private val binding get() = _binding
 
-    var expcoffeQuantity = 0
-    var expcoffeAmount = 200
-
-    var capcoffeQuantity = 0
-    var capcoffeAmount = 250
-
-    var coldCoffeQuantity = 0
-    var coldCoffeAmount = 150
-
-    var hotChocoQuantity = 0
-    var hotChocoAmount = 200
-
-    var mochaCoffeQuantity = 0
-    var mochaCoffeAmount = 300
-    var grandTotal = 0
-
+    //view model declaration
+    lateinit var viewModel: HomePageViewModel
 
 
     override fun onCreateView(
@@ -36,123 +24,114 @@ class HomePageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentHomePageBinding.inflate(inflater, container,false)
+        _binding = FragmentHomePageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
 
-
+    @SuppressLint("SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //view model initialisation
+        viewModel = ViewModelProvider(this)[HomePageViewModel::class.java]
+
         updateLatestValues()
 
         _binding.c1.incrementCountButton1.setOnClickListener {
-            expcoffeQuantity += 1
-            calculateGrandTotal()
+            viewModel.incrementCountButton1()
+            viewModel.grandTotal()
             updateLatestValues()
         }
 
         _binding.c1.decrementCountButton1.setOnClickListener {
-            if(expcoffeQuantity>0){
-                expcoffeQuantity -= 1
-               calculateGrandTotal()
-                updateLatestValues()
-            }
-
+            if (viewModel.expCoffeQuantity > 0)
+                viewModel.decrementCountButton1()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
         _binding.c2.incrementCountButton2.setOnClickListener {
-            capcoffeQuantity += 1
-            calculateGrandTotal()
-             updateLatestValues()
+            viewModel.incrementCountButton2()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
         _binding.c2.decrementCountButton2.setOnClickListener {
-            if (capcoffeQuantity>0){
-                capcoffeQuantity -= 1
-                calculateGrandTotal()
-                updateLatestValues()
-            }
+            if (viewModel.capCoffeQuantity > 0)
+                viewModel.decrementCountButton2()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
         _binding.c3.incrementCountButton3.setOnClickListener {
-            coldCoffeQuantity += 1
-            calculateGrandTotal()
+            viewModel.incrementCountButton3()
+            viewModel.grandTotal()
             updateLatestValues()
         }
 
         _binding.c3.decrementCountButton3.setOnClickListener {
-            if (coldCoffeQuantity>0){
-                coldCoffeQuantity -= 1
-                calculateGrandTotal()
-                updateLatestValues()
-            }
+            if (viewModel.coldCoffeQuantity > 0)
+                viewModel.decrementCountButton3()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
-
         _binding.c4.incrementCountButton4.setOnClickListener {
-            hotChocoQuantity += 1
-            calculateGrandTotal()
+            viewModel.incrementCountButton4()
+            viewModel.grandTotal()
             updateLatestValues()
         }
 
         _binding.c4.decrementCountButton4.setOnClickListener {
-            if (hotChocoQuantity>0){
-                hotChocoQuantity -= 1
-                calculateGrandTotal()
-                updateLatestValues()
-            }
+            if (viewModel.hotChocoQuantity > 0)
+                viewModel.decrementCountButton4()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
         _binding.c5.incrementCountButton5.setOnClickListener {
-            mochaCoffeQuantity += 1
-            calculateGrandTotal()
+            viewModel.incrementCountButton5()
+            viewModel.grandTotal()
             updateLatestValues()
         }
 
         _binding.c5.decrementCountButton5.setOnClickListener {
-            if (mochaCoffeQuantity>0){
-                mochaCoffeQuantity-= 1
-                calculateGrandTotal()
-                updateLatestValues()
-            }
+            if (viewModel.mochaCoffeQuantity > 0)
+                viewModel.decrementCountButton5()
+            viewModel.grandTotal()
+            updateLatestValues()
         }
 
     }
 
-    fun calculateGrandTotal(){
-        val c1Total = expcoffeAmount * expcoffeQuantity
-        val c2Total = capcoffeAmount * capcoffeQuantity
-        val c3Total = coldCoffeAmount * coldCoffeQuantity
-        val c4Total = hotChocoAmount * hotChocoQuantity
-        val c5Total = mochaCoffeAmount * mochaCoffeQuantity
 
-         grandTotal = c1Total + c2Total + c3Total + c4Total + c5Total
+    fun updateLatestValues() {
+
+        _binding.c1.productAmount1.text = viewModel.expCoffeAmount.toString()
+        _binding.c1.expCoffeQuantity.text = viewModel.expCoffeQuantity.toString()
+
+        _binding.c2.productAmount2.text = viewModel.capCoffeAmount.toString()
+        _binding.c2.capCoffeQuantity.text = viewModel.capCoffeQuantity.toString()
+
+        _binding.c3.productAmount3.text = viewModel.coldCoffeAmount.toString()
+        _binding.c3.coldCoffeQuantity.text = viewModel.coldCoffeQuantity.toString()
+
+        _binding.c4.productAmount4.text = viewModel.hotChocoAmount.toString()
+        _binding.c4.hotChocoQuantity.text = viewModel.hotChocoQuantity.toString()
+
+        _binding.c5.productAmount5.text = viewModel.mochaCoffeAmount.toString()
+        _binding.c5.mochaCoffeQuantity.text = viewModel.mochaCoffeQuantity.toString()
+
+        _binding.grandTotal.text = viewModel.grandTotal.toString()
 
     }
 
-    fun updateLatestValues(){
-
-        _binding.c1.productAmount1.text = expcoffeAmount.toString()
-        _binding.c1.quantityCount1.text = expcoffeQuantity.toString()
-
-        _binding.c2.productAmount2.text = capcoffeAmount.toString()
-        _binding.c2.quantityCount2.text = capcoffeQuantity.toString()
-
-        _binding.c3.productAmount3.text = coldCoffeAmount.toString()
-        _binding.c3.quantityCount3.text = coldCoffeQuantity.toString()
-
-        _binding.c4.productAmount4.text = hotChocoAmount.toString()
-        _binding.c4.quantityCount4.text = hotChocoQuantity.toString()
-
-        _binding.c5.productAmount5.text = mochaCoffeAmount.toString()
-        _binding.c5.quantityCount5.text = mochaCoffeQuantity.toString()
-
-
-        _binding.grandTotal.text = grandTotal.toString()
-    }
 
 }
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/brewsoul/HomePageViewmodel.kt
@@ -0,0 +1,75 @@
+package com.example.brewsoul
+
+import androidx.lifecycle.ViewModel
+
+class HomePageViewModel: ViewModel() {
+
+    var expCoffeQuantity = 0
+    var expCoffeAmount = 200
+
+    var capCoffeQuantity = 0
+    var capCoffeAmount = 250
+
+    var coldCoffeQuantity = 0
+    var coldCoffeAmount = 150
+
+    var hotChocoQuantity = 0
+    var hotChocoAmount = 200
+
+    var mochaCoffeQuantity = 0
+    var mochaCoffeAmount = 300
+
+    var grandTotal = 0
+
+
+    fun incrementCountButton1(){
+        expCoffeQuantity +=1
+    }
+
+    fun decrementCountButton1(){
+        expCoffeQuantity -=1
+    }
+
+    fun incrementCountButton2(){
+        capCoffeQuantity +=1
+    }
+
+    fun decrementCountButton2(){
+        capCoffeQuantity -=1
+    }
+
+    fun incrementCountButton3(){
+        coldCoffeQuantity +=1
+    }
+
+    fun decrementCountButton3(){
+        coldCoffeQuantity -=1
+    }
+
+    fun incrementCountButton4(){
+        hotChocoQuantity +=1
+    }
+
+    fun decrementCountButton4(){
+        hotChocoQuantity -=1
+    }
+
+    fun incrementCountButton5(){
+        mochaCoffeQuantity +=1
+    }
+
+    fun decrementCountButton5(){
+        mochaCoffeQuantity -=1
+    }
+
+    fun grandTotal(){
+        val c1Total = expCoffeAmount * expCoffeQuantity
+        val c2Total = capCoffeAmount * capCoffeQuantity
+        val c3Total = coldCoffeAmount * coldCoffeQuantity
+        val c4Total = hotChocoAmount * hotChocoQuantity
+        val c5Total = mochaCoffeAmount * mochaCoffeQuantity
+
+
+        grandTotal = c1Total + c2Total + c3Total + c4Total + c5Total
+    }
+}
